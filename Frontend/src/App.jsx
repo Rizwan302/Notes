7ba5afe424bf17bd282d5513794644da5a92ee6b
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-18 22:53:56 +0500

    Add File

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './compoents/Header'
-import NotesListPage from './pages/NotesListPage'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Header from "./compoents/Header";
+import NotesListPage from "./pages/NotesListPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotePage from "./pages/NotePage";
 
 function App() {
-
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    {/* <Route path="/" element={<Header />} /> */}
-    <Route path="/" element={<NotesListPage />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<NotesListPage />} />
+          <Route path="node/:id" element={<NotePage/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/NotePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotePage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
